dd2963f22d310f56c5027ea48d8678febad2d309
Andrei Smirnov <pinebit@gmail.com>
2015-03-28 15:05:34 +0300

    Inception

---
+++ MyLibrary/MyLibrary.Tests/MyClassTests.cs
@@ -0,0 +1,22 @@
+﻿namespace MyLibrary.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Shouldly;
+
+    [TestClass]
+    public class MyClassTests
+    {
+        [TestMethod]
+        public void ShouldReverseString()
+        {
+            // Arrange
+            MyClass myClass = new MyClass();
+
+            // Act
+            string result = myClass.ReverseString("abc");
+
+            // Assert
+            result.ShouldBe("cba");
+        }
+    }
+}
---
+++ MyLibrary/MyLibrary/MyClass.cs
@@ -0,0 +1,22 @@
+﻿namespace MyLibrary
+{
+    using System;
+
+    /// <summary>
+    /// A sample API that is exposed by this library.
+    /// </summary>
+    public class MyClass
+    {
+        /// <summary>
+        /// Reverse a string.
+        /// </summary>
+        /// <param name="source">A source string.</param>
+        /// <returns>Reversed source string.</returns>
+        public string ReverseString(string source)
+        {
+            char[] charArray = source.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
